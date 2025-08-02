67d88e4e60f6de851f3c4a0e3f0d45fd1a8c1223
jihoon <leejh080628@naver.com>
2025-08-02 20:03:40 +0900

    .

--- HIGHTHON_18/MainViewController.swift
+++ HIGHTHON_18/MainViewController.swift
@@ -9,6 +9,23 @@ class MainViewController: UIViewController {
     private let mainLineImageView = UIImageView().then {
         $0.image = UIImage(named: "line")?.withRenderingMode(.alwaysOriginal)
     }
+    private let upLoadLabel = UILabel().then {
+        $0.text = "PDF를 업로드 해주세요"
+        $0.font = UIFont.systemFont(ofSize: 30, weight: .semibold)
+        $0.textColor = .black
+    }
+    private let loadDetailLabel = UILabel().then {
+        $0.text = "50MB 이하, 50페이지 이내로 올려주세요."
+        $0.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+        $0.textColor = UIColor(named: "mainGray")
+    }
+    
+    
+    private let mainFileImageView = UIImageView().then {
+        $0.image = UIImage(named: "mainFile")?.withRenderingMode(.alwaysOriginal)
+    }
+
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +38,10 @@ class MainViewController: UIViewController {
     func addView() {
         [
             mainLogoImageView,
-            mainLineImageView
+            mainLineImageView,
+            upLoadLabel,
+            mainFileImageView,
+            loadDetailLabel
         ].forEach { view.addSubview($0) }
     }
     
@@ -37,6 +57,25 @@ class MainViewController: UIViewController {
             $0.trailing.equalToSuperview().inset(16)
             $0.width.height.equalTo(24)
         }
+        upLoadLabel.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(110)
+            $0.leading.equalToSuperview().inset(57)
+            $0.trailing.equalToSuperview().inset(56)
+        }
+        loadDetailLabel.snp.makeConstraints {
+            $0.top.equalTo(upLoadLabel.snp.bottom).offset(6)
+            $0.centerX.equalToSuperview()
+//            $0.leading.equalToSuperview().inset(57)
+//            $0.leading.equalToSuperview().inset(56)
+        }
+        mainFileImageView.snp.makeConstraints {
+            $0.top.equalTo(278)
+            $0.leading.equalToSuperview().inset(93)
+            $0.trailing.equalToSuperview().inset(88)
+            $0.width.equalTo(247)
+            $0.height.equalTo(202)
+        }
     }
 }
+
 
